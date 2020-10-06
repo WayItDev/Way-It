04e4e86109d5fd3aa816a736959ef492c3fca16e
tassawer <tassawer@live.nl>
2020-10-06 16:14:52 +0200

    updated signin & signup

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import SignInScreen from './src/screens/SignInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 
 export default class App extends React.Component {
---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import { StyleSheet, View, TextInput, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+export default class SignInScreen extends React.Component {
+  render(){
+
+    state={
+      email:"",
+      password:"",
+      name:""
+    }
+
+    return (
+      <View style= {styles.inputView}>
+        
+        <View style= {styles.containerLogin}>
+            <TouchableOpacity>
+            <Text style={styles.SignInBtn}>SignUp</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+            <Text style={styles.loginBtn}>Log In</Text>
+            </TouchableOpacity>
+        </View>
+
+
+        <TextInput    // ww placeholder
+          style={styles.inputName}
+          placeholder="Name..." 
+          placeholderTextColor="#003f5c"
+          onChangeText={text => this.setState({name:text})}/>
+         
+        <TextInput    // email placeholder
+        style={styles.inputEmail}
+        placeholder="Email..." 
+        placeholderTextColor="#003f5c"
+        onChangeText={text => this.setState({email:text})}/>
+          
+          <TextInput    // ww placeholder
+          style={styles.inputPassword}
+          placeholder="Password..." 
+          placeholderTextColor="#003f5c"
+          onChangeText={text => this.setState({password:text})}/>
+
+            <TouchableOpacity>
+            <Text style={styles.ContBtn}>Continue</Text>
+            </TouchableOpacity>
+
+         <TouchableOpacity>
+          <Text style= {styles.forgot}> Forgot Password? </Text>
+         </TouchableOpacity> 
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+ 
+  inputView: {
+    backgroundColor:"#f1f3f4",  
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',  
+    maxHeight: '90%',
+    
+  },
+
+   containerLogin: {
+    flexDirection: 'row-reverse',
+    justifyContent: 'space-between',
+    paddingVertical: 5,
+
+   }, 
+
+   inputName: {
+    flex:1, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop:50,
+    borderRadius: 50,
+    borderBottomColor: '#3282b8',
+    textShadowColor: '#4d80e4', 
+  }, 
+
+  inputEmail: {
+    flex:2, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop:50,
+    borderRadius: 50,
+    borderBottomColor: '#3282b8',
+    textShadowColor: '#4d80e4', 
+  }, 
+
+  inputPassword: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: "white",
+    marginTop:-50
+  }, 
+
+  forgot:{
+    marginBottom: 25,
+    color:"black",
+    fontSize:11
+  }, 
+
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#665eff",
+    borderRadius:15,
+    height:50,
+    width:60,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:80,
+    marginBottom:10,
+    marginRight:35,
+    color: '#F1F1F1', 
+  },
+  
+  SignInBtn:{
+    width:"80%",
+    backgroundColor:'#665eff',
+    borderRadius:15,
+    height:50,
+    width:60,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:80,
+    marginBottom:10,
+    color: '#F1F1F1',
+  },
+
+  ContBtn:{
+    width:"80%",
+    backgroundColor:'#665eff',
+    borderRadius:15,
+    height:50,
+    width:350,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:80,
+    marginBottom:10,
+    color: '#F1F1F1',
+  },
+
+});
