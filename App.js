912916c3a5151be09481aff6da5786d5866bef9a
tassawer <tassawer@live.nl>
2020-10-11 16:58:13 +0200

    updated

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
+
 
 
 export default class App extends React.Component {
   render(){
     return (
-        <SignInScreen />
+        <SignUpScreen />
     );
   }
 }
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,60 +1,70 @@
 import React from 'react';
 import { StyleSheet, View, TextInput, Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
+import { Button } from 'react-native-elements';
+import { Header } from 'react-native-elements'; 
+
 
 export default class SignInScreen extends React.Component {
   render(){
 
-    state={
-      email:"",
-      password:"",
-      name:""
-    }
-
+  
     return (
+      
       <View style= {styles.inputView}>
-        
-        <View style= {styles.containerLogin}>
-            <TouchableOpacity>
-            <Text style={styles.SignInBtn}>SignUp</Text>
-            </TouchableOpacity>
 
-            <TouchableOpacity>
-            <Text style={styles.loginBtn}>Log In</Text>
-            </TouchableOpacity>
-        </View>
+      <Header
+        centerComponent={{ text: 'Way-It', style: { color: '#373a40', fontSize: 24 } }}
+        containerStyle={{
+          backgroundColor: '#665eff',
+          justifyContent: 'space-around',
+          marginTop: '-40%',
+          marginBottom: '50%'
+        }}
+      /> 
+           <Input
+            placeholder='Email'
+            leftIcon={
+              <Icon
+                name='android'
+                size={21}
+                color='black'
+              />
+            }
+          />
 
+          <Input
+            placeholder='Username'
+            leftIcon={
+              <Icon
+                name='user'
+                size={21}
+                color='black'
+              />
+            }
+          />
 
-        <TextInput    // ww placeholder
-          style={styles.inputName}
-          placeholder="Name..." 
-          placeholderTextColor="#003f5c"
-          onChangeText={text => this.setState({name:text})}/>
-         
-        <TextInput    // email placeholder
-        style={styles.inputEmail}
-        placeholder="Email..." 
-        placeholderTextColor="#003f5c"
-        onChangeText={text => this.setState({email:text})}/>
-          
-          <TextInput    // ww placeholder
-          style={styles.inputPassword}
-          placeholder="Password..." 
-          placeholderTextColor="#003f5c"
-          onChangeText={text => this.setState({password:text})}/>
+          <Input placeholder="Password" secureTextEntry={true}
+            leftIcon={
+              <Icon 
+              name='key'
+              size= {21}
+              color= 'black'
+              />
+                    }
+            />
 
-            <TouchableOpacity>
-            <Text style={styles.ContBtn}>Continue</Text>
-            </TouchableOpacity>
-
-         <TouchableOpacity>
-          <Text style= {styles.forgot}> Forgot Password? </Text>
-         </TouchableOpacity> 
-
-      </View>
-    );
-  }
-}
+              <Button 
+                title="Continue"
+                color= "#665eff"
+                
+              />
+                        
+            </View>
+          );
+        }
+        }
 
 const styles = StyleSheet.create({
 
@@ -64,89 +74,8 @@ const styles = StyleSheet.create({
     flex:1,
     alignItems: 'center',
     justifyContent: 'center',  
-    maxHeight: '90%',
+    maxHeight: '100%',
     
-  },
-
-   containerLogin: {
-    flexDirection: 'row-reverse',
-    justifyContent: 'space-between',
-    paddingVertical: 5,
-
-   }, 
-
-   inputName: {
-    flex:1, 
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop:50,
-    borderRadius: 50,
-    borderBottomColor: '#3282b8',
-    textShadowColor: '#4d80e4', 
-  }, 
-
-  inputEmail: {
-    flex:2, 
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop:50,
-    borderRadius: 50,
-    borderBottomColor: '#3282b8',
-    textShadowColor: '#4d80e4', 
-  }, 
-
-  inputPassword: {
-    flex:1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: "white",
-    marginTop:-50
-  }, 
-
-  forgot:{
-    marginBottom: 25,
-    color:"black",
-    fontSize:11
-  }, 
-
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#665eff",
-    borderRadius:15,
-    height:50,
-    width:60,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:80,
-    marginBottom:10,
-    marginRight:35,
-    color: '#F1F1F1', 
-  },
-  
-  SignInBtn:{
-    width:"80%",
-    backgroundColor:'#665eff',
-    borderRadius:15,
-    height:50,
-    width:60,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:80,
-    marginBottom:10,
-    color: '#F1F1F1',
-  },
-
-  ContBtn:{
-    width:"80%",
-    backgroundColor:'#665eff',
-    borderRadius:15,
-    height:50,
-    width:350,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:80,
-    marginBottom:10,
-    color: '#F1F1F1',
   },
 
 });
